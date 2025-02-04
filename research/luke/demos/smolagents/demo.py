99d5639f30eb8998b741b13c9f0f5e85ad9bbc61
Luke Dewhurst <dewhursl@tcd.ie>
2025-02-04 13:04:31 +0000

    Demo of car salesman scenario.

--- research/luke/demos/smolagents/demo.py
+++ research/luke/demos/smolagents/demo.py
@@ -1,15 +1,71 @@
-from litellm import completion
+from smolagents import CodeAgent, ManagedAgent, LiteLLMModel, tool
 
-response = completion(
-    model = "ollama/llama2",
-    messages = [{
-        "content": "Hello, how are you?",
-        "role": "user"
-    }],
-    api_base = "http://localhost:11434",
-    stream = True
+model = LiteLLMModel(
+    model_id = "ollama/llama2",
+    num_ctx = 8192
 )
 
-for chunk in response:
-    print(chunk.choices[0].delta.content, end = '', flush = True)
-print()
+p_price: int = 40000
+
+@tool
+def get_price() -> int:
+    """
+    Returns the current price of the car.
+    """
+    return p_price
+
+@tool
+def set_price(price: int) -> None:
+    """
+    Sets the current price of the car.
+
+    Args:
+        price: Updated price.
+    """
+    p_price = price
+
+seller = CodeAgent(
+    tools = [get_price, set_price], 
+    model = model, 
+    add_base_tools = True
+)
+
+buyer = CodeAgent(
+    tools = [get_price], 
+    model = model, 
+    add_base_tools = True
+)
+
+managed_buyer = ManagedAgent(
+    agent = buyer,
+    name = "buyer",
+    description = """
+        Represents a person who wants to purchase a new car.
+    """
+)
+
+managed_seller = ManagedAgent(
+    agent = seller,
+    name = "seller",
+    description = """
+        Represents the seller for the new car.
+    """
+)
+
+manager = CodeAgent(
+    tools = [],
+    model = model,
+    managed_agents = [
+        managed_buyer,
+        managed_seller
+    ]
+)
+
+manager.run(
+    """
+    Can you simulate a buyer negotiating the price of a car with a seller using your managed buyer 
+    and seller agents?
+    """
+)
+
+print(p_price)
