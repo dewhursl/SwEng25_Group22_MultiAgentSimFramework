139ff86edd408ffd917d0115aea30847d44c7463
Luke Dewhurst <dewhursl@tcd.ie>
2025-02-07 15:22:42 +0000

    Update PixiJS demo to read in agent position and texture from JSON.

--- research/luke/demos/pixijs/modules/simulation.js
+++ research/luke/demos/pixijs/modules/simulation.js
@@ -1,13 +1,33 @@
+// Read in simulation state from JSON
+const simulation = await fetch('./json/simulation.json')
+    .then(response => response.json())
+    .catch(error => console.error('Error loading JSON: ', error));
+
+// Initialize application
 const app = new PIXI.Application();
 await app.init({ width: 640, height: 360 });
 document.body.appendChild(app.canvas);
 
-await PIXI.Assets.load('images/sample.png');
-const sprite = PIXI.Sprite.from('images/sample.png');
-app.stage.addChild(sprite);
+// Initialize agents
+simulation.agents.forEach(async (agent) => {
+    const texture = 'textures/' + agent.texture;
 
+    await PIXI.Assets.load(texture);
+    agent.sprite = PIXI.Sprite.from(texture);
+
+    agent.sprite.x = agent.position.x;
+    agent.sprite.y = agent.position.y;
+
+    app.stage.addChild(agent.sprite);
+});
+
+// Animate Rendering
 let elapsed = 0.0;
+
 app.ticker.add((ticker) => {
     elapsed += ticker.deltaTime;
-    sprite.x = 100 + Math.cos(elapsed / 50.0) * 100;
+
+    simulation.agents.forEach(async agent => {
+        agent.sprite.x = agent.position.x + Math.cos(elapsed / 50.0) * 100;
+    })
 });
