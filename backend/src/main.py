1a505b0c55b031aca800c447335d4f3d54e9f686
Timofey Cvetkov <cvetkovt@tcd.ie>
2025-02-21 16:46:47 +0000

    Working Monte Carlo simulation

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,15 +1,15 @@
+import asyncio
+
 from dotenv import load_dotenv
 load_dotenv()
 
-from sim.simulation import Simulation
+from sim.monte_carlo import MonteCarloSimulator
 
 
 def main():
-    print("Initialising Simulation")
-    sim = Simulation("car_sale_simulation.json")
-
-    print("Running Simulation")
-    sim.run()
+    monte_carlo = MonteCarloSimulator("car_sale_simulation.json", num_runs=2)
+    results = asyncio.run(monte_carlo.run_monte_carlo())
+    print(results)
 
 
 if __name__ == "__main__":
--- backend/src/sim/simulation.py
+++ backend/src/sim/simulation.py
@@ -131,8 +131,8 @@ class Simulation:
             )
         )
 
-    def run(self):
-        asyncio.run(Console(self.gc.run_stream()))
+    async def run(self):
+        return await Console(self.gc.run_stream())
 
     #with DAG(
     #"simulation_task",
@@ -144,38 +144,4 @@ class Simulation:
     #    task_id="sim_task",
     #    python_callable=run,
     #)
-    #sim_task  
-
-
-
-    # def parse_output_variables(self):
-    #     results = {}
-        
-    #     lines = message.splitlines()
-    #     filtered_lines = []
-        
-    #     for line in lines:
-    #         if not any(re.search(rf"{vname}\s*=", line) for vname in output_variables):
-    #             filtered_lines.append(line)
-            
-    #     full_match = True
-    #     partial_match = False
-
-    #     for vname, vtype in output_variables.items():
-    #         str_pattern = rf"{vname}\s*=\s*\"(.*?)\""
-    #         num_pattern = rf"{vname}\s*=\s*(\d+)"
-            
-    #         str_match = re.search(str_pattern, message)
-    #         num_match = re.search(num_pattern, message)
-            
-    #         if vtype == "String":
-    #             results[vname] = str_match.group(1) if str_match else None
-    #         elif vtype == "Number":
-    #             results[vname] = int(num_match.group(1)) if num_match else None
-
-    #         if results[vname] == None:
-    #             full_match = False
-    #         else:
-    #             partial_match = True
-        
-    #     return results, partial_match, full_match
+    #sim_task
