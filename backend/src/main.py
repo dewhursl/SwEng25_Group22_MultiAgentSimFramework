5957ba27a6f8921e5db6c5c45028719b4bbd3353
CloudyUnity <finn.wright5@gmail.com>
2025-02-21 16:25:23 +0000

    Cleaned up some files

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,16 +1,17 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from sim.simulation import Simulation
+import ipdb
 
+from sim.simulation import Simulation
 
 def main():
     print("Initialising Simulation")
     sim = Simulation("car_sale_simulation.json")
 
-    print("Running Simulation")
-    #sim.run()
-    import ipdb; ipdb.set_trace()
+    ipdb.set_trace()
+    
+    print("Running Simulation")    
     sim.run()
 
 if __name__ == "__main__":
--- backend/src/sim/simulation.py
+++ backend/src/sim/simulation.py
@@ -15,77 +15,19 @@ from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.conditions import MaxMessageTermination, TextMentionTermination
 from autogen_agentchat.teams import SelectorGroupChat
 from autogen_agentchat.ui import Console
-from autogen_ext.models.openai import OpenAIChatCompletionClient
 
-def init_gpt_model_client():
-    return OpenAIChatCompletionClient(
-            model="gpt-4o",
-            api_key=os.environ["OPENAI_API_KEY"]
-        )   
-
-def init_together_model_client():
-    return OpenAIChatCompletionClient(
-                model="meta-llama/Llama-3.3-70B-Instruct-Turbo",
-                base_url="https://api.together.xyz/v1",
-                api_key=os.environ["TOGETHER_API_KEY"],
-                model_info={
-                    "vision": False,
-                    "function_calling": True,
-                    "json_output": False,
-                    "family": "llama",
-                },
-            )
-
-def init_ollama_model_client():
-    return OpenAIChatCompletionClient(
-            model="llama3.2:latest",
-            base_url="http://localhost:11434/v1",
-            api_key="placeholder",
-            model_info={
-                "vision": False, 
-                "function_calling": True, 
-                "json_output": False, 
-                "family": "llama", 
-            },
-        )
-
-def select_model(type: str, gpt_model, together_model, ollama_model):
-    if (type == "GPT"):
-        return gpt_model
-    elif (type == "Together"):
-        return together_model
-    elif (type == "Ollama"):
-        return ollama_model
-    raise Exception("Invalid model type: " + type)
+import model_client
 
 class Simulation:
     def __init__(self, sim_config_file_name, max_messages=25):        
 
-        print("Loading config file: " + sim_config_file_name)
-        
+        print("Loading config file: " + sim_config_file_name)        
         self.config = SimConfigLoader(sim_config_file_name).load()        
 
         print("Initialising model client")
-
-        models = self.config['models']
-        model_list = [ models["agent"], models["info_return_agent"], models["group_chat"]] # Can this be simplified?
-        gpt_model_client = None
-        together_model_client = None
-        ollama_model_client = None
-
-        if (model_list.__contains__("GPT")):
-            gpt_model_client = init_gpt_model_client()
-        if (model_list.__contains__("Together")):
-            together_model_client = init_together_model_client()
-        if (model_list.__contains__("Ollama")):
-            ollama_model_client = init_ollama_model_client()
-
-        self.agent_model_client = select_model(models["agent"], gpt_model_client, together_model_client, ollama_model_client)
-        self.info_model_client = select_model(models["info_return_agent"], gpt_model_client, together_model_client, ollama_model_client)
-        self.gc_model_client = select_model(models["group_chat"], gpt_model_client, together_model_client, ollama_model_client)
+        model_client.init_model_clients(self)
 
         print("Setting up AutoGen Agents")
-
         self.agents = [
             AssistantAgent(
                 agent["name"],
@@ -96,7 +38,6 @@ class Simulation:
         ]
         
         print("Setting up InformationReturnAgent")
-
         output_variables_str="\n".join([f"{v['name']} # {v['type']}" for v in self.config['output_variables']])
 
         information_return_agent = AssistantAgent(
@@ -115,7 +56,6 @@ class Simulation:
         self.agents.append(information_return_agent)
 
         print("Setting up SelectorGroupChat")
-
         self.gc = SelectorGroupChat(
             self.agents,
             model_client=self.gc_model_client,
