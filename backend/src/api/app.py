cfa2701604dfdcbf4f11c7d149c48adaca8ef651
Fiona Wright <finn.wright5@gmail.com>
2025-03-09 20:24:43 +0000

    Config Generator WIP, mostly works

--- backend/src/api/app.py
+++ backend/src/api/app.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from api.routes.results import results_bp
 from api.routes.create import create_bp
-from api.routes.generate import generate_bp
 from api.routes.catalog import catalog_bp
+from api.routes.gen_config import gen_config_bp
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -11,5 +11,5 @@ app = Flask(__name__)
 
 app.register_blueprint(results_bp, url_prefix="/sim")
 app.register_blueprint(create_bp, url_prefix="/sim")
-app.register_blueprint(generate_bp, url_prefix="/sim")
 app.register_blueprint(catalog_bp, url_prefix="/sim")
+app.register_blueprint(gen_config_bp, url_prefix="/sim")
--- backend/src/api/routes/generate.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-generate_bp = Blueprint("generate", __name__)
-
-@generate_bp.route("/generate", methods=["POST", "PUT"])
-def generate_simulation_config():
-    return jsonify({"message": "Not implemented."}), 501
