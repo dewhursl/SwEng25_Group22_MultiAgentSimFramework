0a7cb7d5d95f25d083c5e031e7ab25e426a7a5b4
AineHyndman <hyndmana@tcd.ie>
2025-04-07 13:56:43 +0100

    cleaned up dead code in simulation.py

--- backend/src/OLD/sim/simulation.py
+++ backend/src/OLD/sim/simulation.py
@@ -124,39 +124,6 @@ class Simulation:
     async def run(self):
         return await Console(self.gc.run_stream())
 
-
-    # def parse_output_variables(self):
-    #     results = {}
-        
-    #     lines = message.splitlines()
-    #     filtered_lines = []
-        
-    #     for line in lines:
-    #         if not any(re.search(rf"{vname}\s*=", line) for vname in output_variables):
-    #             filtered_lines.append(line)
-            
-    #     full_match = True
-    #     partial_match = False
-
-    #     for vname, vtype in output_variables.items():
-    #         str_pattern = rf"{vname}\s*=\s*\"(.*?)\""
-    #         num_pattern = rf"{vname}\s*=\s*(\d+)"
-            
-    #         str_match = re.search(str_pattern, message)
-    #         num_match = re.search(num_pattern, message)
-            
-    #         if vtype == "String":
-    #             results[vname] = str_match.group(1) if str_match else None
-    #         elif vtype == "Number":
-    #             results[vname] = int(num_match.group(1)) if num_match else None
-
-    #         if results[vname] == None:
-    #             full_match = False
-    #         else:
-    #             partial_match = True
-        
-    #     return results, partial_match, full_match
-
 if __name__ == "__main__":
     sim = Simulation("backend/src/sim_configs/sim_config.json")  
     asyncio.run(sim.run())
